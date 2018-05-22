ccc8ff359ce644835a771ddfaab3fb3d002f0222
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-22 12:27:00 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,14 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
+
 
   def initialize(name)
     @name = name
+    @back_projects = []
+  end
+
+  def back_project(title)
+    self.back_projects << title
   end
 
 end
